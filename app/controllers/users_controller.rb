3b88dd64d0666ccfaa8115e46004155449d3379e
bonot9434 <bonot9434@gmail.com>
2021-08-18 12:20:03 +0000

    [add]アクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,38 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
   end
   
   def show
     @user = User.find(params[:id])
-    @books = Book.where(user_id: @user.id)
-   
+    @book = Book.new
+    @books = @user.books.page(params[:page]).reverse_order
   end
 
   def edit
-
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+       render:edit
+    end
   end
   
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction_id)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7dbd862e5d8754b31f47a0328c117011aca1358ae5d2f9619b104e202f945a54971e38b63729e77242c9e34db197d4ea0bdaa08b835e6734cc26296884bbd777'
